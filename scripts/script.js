a494abad49db96200b58370e1a1db52bfeb6a0ab
Sean Wallace <wallace.sean111@gmail.com>
2022-09-18 05:14:39 +0200

    Wrote calculator function

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,11 @@
 let bill = 0;
 let tipPercent = 0;
+let customTip = 0;
 let billPlusTip = 0;
 let numberPeople = 0;
 let totalTip = 0;
 let tipPerPerson = 0;
 let totalPerPerson = 0;
-let customTip = 0;
 
 (function () {
   console.log("function active");
@@ -26,20 +26,25 @@ function updateValue(value, id) {
     if (id === "bill-input") {
       bill = value;
       bill = Number(bill);
-      billCents = bill * 100;
-      console.log(typeof(bill));
+      
     } else if (id === "custom-input") {
       customTip = value;
-      customTip = Number(customTip);
-      console.log(typeof(customTip));
+      tipPercent = customTip;
+      tipPercent = Number(tipPercent);
+
     } else {
       numberPeople = value;
       numberPeople = Number(numberPeople);
-      console.log(typeof(numberPeople));
+      
     }
-    calculator(billCents, tipPercent, numberPeople);
+    calculator(bill, tipPercent, numberPeople);
 }
 
-function calculator(billCents, tipPercent, numberPeople){
-  
+function calculator(bill, tipPercent, numberPeople){
+  totalTip = bill * (tipPercent / 100);
+  tipPerPerson = totalTip / numberPeople;
+  totalPerPerson = (bill + totalTip) / numberPeople;
+  console.log("totalTip is", totalTip);
+  console.log("tipPerPerson is", tipPerPerson);
+  console.log("totalPerPerson is", totalPerPerson);
 }
